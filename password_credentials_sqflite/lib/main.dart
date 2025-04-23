f4ef425186b9ecb7260a921c9fcbc5165c234479
deepak-nayak <deepaknayak55260@gmail.com>
2025-04-23 20:21:18 +0530

    Debug tag removed and margin reduced for error msg

--- password_credentials_sqflite/lib/main.dart
+++ password_credentials_sqflite/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
           seedColor: const Color.fromARGB(248, 92, 4, 243),
